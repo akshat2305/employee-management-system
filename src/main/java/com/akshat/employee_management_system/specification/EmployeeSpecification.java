2e29577b8d0420cfa14e083e42e22383f9b785ff
Akshat Khanna <akshat23khanna@gmail.com>
2026-07-08 00:56:11 +0530

    feat: add department specification for dynamic employee filtering

--- src/main/java/com/akshat/employee_management_system/specification/EmployeeSpecification.java
+++ src/main/java/com/akshat/employee_management_system/specification/EmployeeSpecification.java
@@ -1,5 +1,20 @@
 package com.akshat.employee_management_system.specification;
+import com.akshat.employee_management_system.entity.Employee;
+import org.springframework.data.jpa.domain.Specification;
 
 public class EmployeeSpecification {
+    public static Specification<Employee> hasDepartment(String department) {
 
+        return (root, query, criteriaBuilder) -> {
+
+            if (department == null || department.isBlank()) {
+                return null;
+            }
+
+            return criteriaBuilder.equal(
+                    root.get("department"),
+                    department
+            );
+        };
+    }
 }
